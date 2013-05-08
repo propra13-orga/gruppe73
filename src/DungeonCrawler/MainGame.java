eac64395495c2db5e5ff6a426d4b928df3d53f25
Sarah Conen <sarah.conen@hhu.de>
2013-05-08 15:16:27 +0200

    erster

---
+++ src/DungeonCrawler/MainGame.java
@@ -0,0 +1,14 @@
+package DungeonCrawler;
+
+public class MainGame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hallo Sarah!");
+
+	}
+
+}
