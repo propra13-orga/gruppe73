417b91108901e3ec4cebe2ebf6458da4030c7dea
Sarah Conen <sarah.conen@hhu.de>
2013-06-09 13:03:57 +0200

    Kommentar hinzugefügt

--- src/dungeoncrawler/Player.java
+++ src/dungeoncrawler/Player.java
@@ -3,9 +3,9 @@ package dungeoncrawler;
 public class Player {
 	public static int PlayerSword = 0;		// Anzahl der Schwerter, die der Player besitzt
 	public static int Medikit = 0;			//  ''    ''  Medikits, ''
-	public static int CurrentPoints = 0;
-	public static int Lives;
-	public static int Manadrinks;
+	public static int CurrentPoints = 0;	//Anzahl der Punkte - wird beim Gewinn des Schatzes o.ä hoch gesetzt
+	public static int Lives = 1;			//Anzahl Leben
+	public static int Manadrinks;			//
 	public static int Bow;
 	public static int priceManadrinks = 100;
 	public static int prizeTreasure = 500;
