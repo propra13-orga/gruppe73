68d65571902398a93b246976f22a1990519038f0
Sarah Conen <sarah.conen@hhu.de>
2013-06-09 13:12:07 +0200

    Items des Players definiert

--- src/dungeoncrawler/Player.java
+++ src/dungeoncrawler/Player.java
@@ -5,11 +5,15 @@ public class Player {
 	public static int Medikit = 0;			//  ''    ''  Medikits, ''
 	public static int CurrentPoints = 0;	//Anzahl der Punkte - wird beim Gewinn des Schatzes o.ä hoch gesetzt
 	public static int Lives = 1;			//Anzahl Leben
-	public static int Manadrinks;			//
-	public static int Bow;
+	public static int Manadrinks = 0;		// " Manadrinks (ermöglichen das Zaubern)
+	public static int Bow = 0;				// " Bögen
+	public static int Arrow = 0;			// " Pfeile
+	public static int suitofarmor = 0;  	// " Rüstungen
 	public static int priceManadrinks = 100;
-	public static int prizeTreasure = 500;
 	public static int priceMedikit = 350; 
 	public static int priceSword = 200;
+	public static int priceBow = 200;
+	public static int priceArrow = 100; 	// 100 Taler pro Stück!
+	public static int pricesuitofarmor = 600;
 
 }
