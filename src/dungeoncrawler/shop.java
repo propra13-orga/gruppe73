d95b1960cce625f798913fd05c2be93191459243
Sarah Conen <sarah.conen@hhu.de>
2013-06-13 16:17:21 +0200

    shop mit main methode

--- src/dungeoncrawler/shop.java
+++ src/dungeoncrawler/shop.java
@@ -1,28 +1,45 @@
 package dungeoncrawler;
 
-import javax.swing.JPanel;
+import java.awt.EventQueue;
+
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JTextField;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JFrame;
 
-public class shop extends JPanel {
+
+public class Shop extends JFrame {
+
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JLabel lblHealth;
 
 	/**
-	 * Create the panel.
+	 * Launch the application.
 	 */
-	public shop() {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Shop frame = new Shop();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Shop() {
 		setBounds(300, 300, 400, 320);
 		setLayout(null);
 		
@@ -32,11 +49,11 @@ public class shop extends JPanel {
 		add(lblShop);
 		
 		JLabel BildMedikit = new JLabel("");
-		BildMedikit.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/Medikit.PNG")));
+		BildMedikit.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/Medikit.PNG")));
 		BildMedikit.setBounds(20, 50, 14, 14);
 		add(BildMedikit);
 		
-		lblHealth = new JLabel("Medikit");
+		JLabel lblHealth = new JLabel("Medikit");
 		lblHealth.setFont(new Font("Stencil", Font.PLAIN, 11));
 		lblHealth.setBounds(43, 47, 46, 23);
 		add(lblHealth);
@@ -52,7 +69,7 @@ public class shop extends JPanel {
 			public void mouseClicked(MouseEvent e) {
 			}
 		});
-		PlusMedikit.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/plus.PNG")));
+		PlusMedikit.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/plus.PNG")));
 		PlusMedikit.setBounds(126, 50, 14, 14);
 		add(PlusMedikit);
 		
@@ -62,7 +79,7 @@ public class shop extends JPanel {
 			public void mouseClicked(MouseEvent e) {
 			}
 		});
-		MinusMedikit.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/minus.PNG")));
+		MinusMedikit.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/minus.PNG")));
 		MinusMedikit.setBounds(149, 50, 14, 14);
 		add(MinusMedikit);
 		
@@ -72,7 +89,7 @@ public class shop extends JPanel {
 		add(PreisMedikit);
 		
 		JLabel MuenzeMedikit = new JLabel("");
-		MuenzeMedikit.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/points.PNG")));
+		MuenzeMedikit.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/points.PNG")));
 		MuenzeMedikit.setBounds(194, 50, 14, 14);
 		add(MuenzeMedikit);
 		
@@ -110,7 +127,7 @@ public class shop extends JPanel {
 			public void mouseClicked(MouseEvent e) {
 			}
 		});
-		PlusRuestung.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/plus.PNG")));
+		PlusRuestung.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/plus.PNG")));
 		PlusRuestung.setBounds(126, 77, 14, 14);
 		add(PlusRuestung);
 		
@@ -120,7 +137,7 @@ public class shop extends JPanel {
 			public void mouseClicked(MouseEvent e) {
 			}
 		});
-		PlusMana.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/plus.PNG")));
+		PlusMana.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/plus.PNG")));
 		PlusMana.setBounds(126, 102, 14, 14);
 		add(PlusMana);
 		
@@ -130,7 +147,7 @@ public class shop extends JPanel {
 			public void mouseClicked(MouseEvent e) {
 			}
 		});
-		MinusRuestung.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/minus.PNG")));
+		MinusRuestung.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/minus.PNG")));
 		MinusRuestung.setBounds(149, 77, 14, 14);
 		add(MinusRuestung);
 		
@@ -140,17 +157,17 @@ public class shop extends JPanel {
 			public void mouseClicked(MouseEvent e) {
 			}
 		});
-		MinusMana.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/minus.PNG")));
+		MinusMana.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/minus.PNG")));
 		MinusMana.setBounds(150, 102, 14, 14);
 		add(MinusMana);
 		
 		JLabel BildRüstung = new JLabel("");
-		BildRüstung.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/ruestung.PNG")));
+		BildRüstung.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/ruestung.PNG")));
 		BildRüstung.setBounds(20, 77, 14, 14);
 		add(BildRüstung);
 		
 		JLabel BildMana = new JLabel("");
-		BildMana.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/Mana.PNG")));
+		BildMana.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/Mana.PNG")));
 		BildMana.setBounds(20, 102, 14, 14);
 		add(BildMana);
 		
@@ -165,7 +182,7 @@ public class shop extends JPanel {
 		add(PreisMana);
 		
 		JLabel BildSchwert = new JLabel("");
-		BildSchwert.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/Sword.PNG")));
+		BildSchwert.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/Sword.PNG")));
 		BildSchwert.setBounds(20, 127, 14, 14);
 		add(BildSchwert);
 		
@@ -185,12 +202,12 @@ public class shop extends JPanel {
 			public void mouseClicked(MouseEvent arg0) {
 			}
 		});
-		PlusSchwert.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/plus.PNG")));
+		PlusSchwert.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/plus.PNG")));
 		PlusSchwert.setBounds(126, 127, 14, 14);
 		add(PlusSchwert);
 		
 		JLabel MinusSchwert = new JLabel("");
-		MinusSchwert.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/minus.PNG")));
+		MinusSchwert.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/minus.PNG")));
 		MinusSchwert.setBounds(149, 127, 14, 14);
 		add(MinusSchwert);
 		
@@ -200,22 +217,22 @@ public class shop extends JPanel {
 		add(PreisSchwert);
 		
 		JLabel MuenzeRuestung = new JLabel("");
-		MuenzeRuestung.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/points.PNG")));
+		MuenzeRuestung.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/points.PNG")));
 		MuenzeRuestung.setBounds(194, 77, 14, 14);
 		add(MuenzeRuestung);
 		
 		JLabel MuenzeMana = new JLabel("");
-		MuenzeMana.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/points.PNG")));
+		MuenzeMana.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/points.PNG")));
 		MuenzeMana.setBounds(194, 102, 14, 14);
 		add(MuenzeMana);
 		
 		JLabel MunezeSchwert = new JLabel("");
-		MunezeSchwert.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/points.PNG")));
+		MunezeSchwert.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/points.PNG")));
 		MunezeSchwert.setBounds(194, 127, 14, 14);
 		add(MunezeSchwert);
 		
 		JLabel BildBogen = new JLabel("");
-		BildBogen.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/bogen.png")));
+		BildBogen.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/bogen.png")));
 		BildBogen.setBounds(20, 152, 14, 14);
 		add(BildBogen);
 		
@@ -230,12 +247,12 @@ public class shop extends JPanel {
 		add(AnzahlBogen);
 		
 		JLabel PlusBogen = new JLabel("");
-		PlusBogen.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/plus.PNG")));
+		PlusBogen.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/plus.PNG")));
 		PlusBogen.setBounds(126, 152, 14, 14);
 		add(PlusBogen);
 		
 		JLabel MinusBogen = new JLabel("");
-		MinusBogen.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/minus.PNG")));
+		MinusBogen.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/minus.PNG")));
 		MinusBogen.setBounds(149, 152, 14, 14);
 		add(MinusBogen);
 		
@@ -245,12 +262,12 @@ public class shop extends JPanel {
 		add(PreisBogen);
 		
 		JLabel MuenzeBogen = new JLabel("");
-		MuenzeBogen.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/points.PNG")));
+		MuenzeBogen.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/points.PNG")));
 		MuenzeBogen.setBounds(194, 152, 14, 14);
 		add(MuenzeBogen);
 		
 		JLabel BildPfeil = new JLabel("");
-		BildPfeil.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/pfeil1.png")));
+		BildPfeil.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/pfeil1.png")));
 		BildPfeil.setBounds(20, 177, 14, 14);
 		add(BildPfeil);
 		
@@ -260,7 +277,7 @@ public class shop extends JPanel {
 		add(AnzahlPfeil);
 		
 		JLabel PlusPfeil = new JLabel("");
-		PlusPfeil.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/plus.PNG")));
+		PlusPfeil.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/plus.PNG")));
 		PlusPfeil.setBounds(126, 177, 14, 14);
 		add(PlusPfeil);
 		
@@ -270,7 +287,7 @@ public class shop extends JPanel {
 		add(lblPfeil);
 		
 		JLabel MinusPfeil = new JLabel("");
-		MinusPfeil.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/minus.PNG")));
+		MinusPfeil.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/minus.PNG")));
 		MinusPfeil.setBounds(149, 177, 14, 14);
 		add(MinusPfeil);
 		
@@ -280,7 +297,7 @@ public class shop extends JPanel {
 		add(PreisPfeil);
 		
 		JLabel MuenzePfeil = new JLabel("");
-		MuenzePfeil.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/points.PNG")));
+		MuenzePfeil.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/points.PNG")));
 		MuenzePfeil.setBounds(194, 177, 14, 14);
 		add(MuenzePfeil);
 		
@@ -313,12 +330,12 @@ public class shop extends JPanel {
 		add(lblNewLabel_1);
 		
 		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/Medikit.PNG")));
+		label.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/Medikit.PNG")));
 		label.setBounds(20, 0, 14, 14);
 		add(label);
 		
 		JLabel label_1 = new JLabel("");
-		label_1.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/ruestung.PNG")));
+		label_1.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/ruestung.PNG")));
 		label_1.setBounds(60, 0, 14, 14);
 		add(label_1);
 		
@@ -333,12 +350,12 @@ public class shop extends JPanel {
 		add(label_3);
 		
 		JLabel label_4 = new JLabel("");
-		label_4.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/Mana.PNG")));
+		label_4.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/Mana.PNG")));
 		label_4.setBounds(100, 0, 14, 14);
 		add(label_4);
 		
 		JLabel label_5 = new JLabel("");
-		label_5.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/Sword.PNG")));
+		label_5.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/Sword.PNG")));
 		label_5.setBounds(140, 0, 14, 14);
 		add(label_5);
 		
@@ -353,7 +370,7 @@ public class shop extends JPanel {
 		add(label_7);
 		
 		JLabel label_8 = new JLabel("");
-		label_8.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/bogen.png")));
+		label_8.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/bogen.png")));
 		label_8.setBounds(180, 0, 14, 14);
 		add(label_8);
 		
@@ -363,7 +380,7 @@ public class shop extends JPanel {
 		add(label_9);
 		
 		JLabel label_10 = new JLabel("");
-		label_10.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/pfeil1.png")));
+		label_10.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/pfeil1.png")));
 		label_10.setBounds(220, 0, 14, 14);
 		add(label_10);
 		
@@ -374,20 +391,9 @@ public class shop extends JPanel {
 		
 		JLabel lblShopBackground = new JLabel("");
 		lblShopBackground.setBackground(Color.WHITE);
-		lblShopBackground.setIcon(new ImageIcon(shop.class.getResource("/dungeoncrawler/Inventar.PNG")));
+		lblShopBackground.setIcon(new ImageIcon(Shop.class.getResource("/dungeoncrawler/Inventar.PNG")));
 		lblShopBackground.setBounds(0, 0, 400, 320);
 		add(lblShopBackground);
-		
-		/*
-		public static void main(String[] args) {
-			final shop m = new shop();
-			javax.swing.SwingUtilities.invokeLater(new Runnable() {
-	        	public void run() {
-	        		m.setVisible(true);
-				}
-			});
-		
-		*/
-
 	}
+
 }
