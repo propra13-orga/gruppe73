c530ae8167b1101ccf55a42359ca0c8569e8840a
Sarah Conen <sarah.conen@hhu.de>
2013-06-24 14:53:05 +0200

    ungenutzen import entfernt

--- src/dungeoncrawler/StoryNPC.java
+++ src/dungeoncrawler/StoryNPC.java
@@ -5,7 +5,6 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
