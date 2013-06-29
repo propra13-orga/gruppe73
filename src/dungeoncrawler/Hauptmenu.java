a16eb33217a88813ee62e6605b695febb33b7c48
Sarah Conen <sarah.conen@hhu.de>
2013-06-29 13:57:09 +0200

    Beginn Netzwerk

--- src/dungeoncrawler/Hauptmenu.java
+++ src/dungeoncrawler/Hauptmenu.java
@@ -3,6 +3,8 @@ import java.awt.event.ActionEvent;
 import dungeoncrawler.BuildLevel;
 import java.awt.event.ActionListener;
 import dungeoncrawler.Shop;
+import Network.ClientMain;
+import Network.ServerMain;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -19,6 +21,8 @@ public class Hauptmenu extends JFrame {
 	JButton button_start;
 	JButton button_ende;
 	JButton button_shop;
+	JButton button_networkServer;
+	JButton button_networkClient;
 	
 	
 	public Hauptmenu() {
@@ -34,18 +38,24 @@ public class Hauptmenu extends JFrame {
 		//Label- und Buttoneigenschaften setzen / positionieren
 		label_name = new JLabel(icon_bild);
 		label_name.setBounds(0, 0, 600, 400);
-		button_start = new JButton("Spiel starten");
-		button_start.setBounds(300, 200, 150, 50);
+		button_start = new JButton("Einzelspiel starten");
+		button_start.setBounds(280, 200, 185, 30);
 		button_ende = new JButton("Spiel beenden");
-		button_ende.setBounds(300, 270, 150, 50);
+		button_ende.setBounds(280, 320, 185, 30);
 		button_shop = new JButton("Shop");
 		button_shop.setBounds(100, 270, 120, 35);
+		button_networkServer = new JButton("Multipayer starten (Host)");
+		button_networkServer.setBounds(280, 240, 185, 30);
+		button_networkClient = new JButton("Multiplayer beitreten");
+		button_networkClient.setBounds(280, 280, 185, 30);
 
 		//Label und Buttons hinzufuegen
 
 		add(button_start);
 		add(button_ende);
-		add(button_shop);
+		//add(button_shop);
+		add(button_networkClient);
+		add(button_networkServer);
 		add(label_name);
 		button_start.setVisible(true);
 		button_ende.setVisible(true);
@@ -80,6 +90,22 @@ public class Hauptmenu extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);          //Programm beenden
+			}
+			
+		});
+		button_networkClient.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ClientMain.main(null);          //Programm beenden
+			}
+			
+		});
+		button_networkServer.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ServerMain.main(null);          //Programm beenden
 			}
 			
 		});
