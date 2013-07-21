5b2d0b7d90677ed1048fbb70ada523effb35afbe
Nils <nils.schmidt@uni-duesseldorf.de>
2013-07-21 21:25:19 +0200

    Laden von spielständen

--- src/Spielstaende/Speichern.java
+++ src/Spielstaende/Speichern.java
@@ -25,20 +25,17 @@ public class Speichern {
 		String Dateiname = JOptionPane.showInputDialog(null, "Bitte gib deinen Namen ein");
 		Dateiname = Dateiname + ".TXT";
 		System.out.println(Dateiname);
-		
-		
-		
-		if(MovementListener.gespeichert == true){
-			try{
-				String FilePath = System.getProperty("user.dir");
-				@SuppressWarnings("resource")
-				PrintWriter pWriter = new PrintWriter(new FileWriter(FilePath+"/src/dungeoncrawler/"+Dateiname));
-				pWriter.println(BuildLevel.Current_Level+"\n" + BuildLevel.getCurrentPlayerPos(0)+"\n" + BuildLevel.getCurrentPlayerPos(1) +"\n" + Player.AktuelleWaffe + "\n" + Player.Arrow + "\n" + Player.Bow + "\n" + Player.CurrentPoints + "\n" + Player.Lives + "\n" + Player.Manadrinks + "\n" + Player.Medikit + "\n" + Player.PlayerPower + "\n" + Player.PlayerSword + "\n");
-				pWriter.flush();
-				JOptionPane.showMessageDialog(null , "Datei wurde gespeichert.", "Datei wurde gespeichert!", JOptionPane.INFORMATION_MESSAGE);
-			}catch(IOException ioe){
-				JOptionPane.showMessageDialog(null, "Datei konnte nicht gespeichert werden.", "Errror", JOptionPane.ERROR_MESSAGE, null);
-		}
+	
+		try{
+			String FilePath = System.getProperty("user.dir");
+			@SuppressWarnings("resource")
+			PrintWriter pWriter = new PrintWriter(new FileWriter(FilePath+"/src/dungeoncrawler/"+Dateiname));
+			pWriter.println(BuildLevel.Current_Level+"\n" + BuildLevel.getCurrentPlayerPos(0)+"\n" + BuildLevel.getCurrentPlayerPos(1) +"\n" + Player.AktuelleWaffe + "\n" + Player.Arrow + "\n" + Player.Bow + "\n" + Player.CurrentPoints + "\n" + Player.Lives + "\n" + Player.Manadrinks + "\n" + Player.Medikit + "\n" + Player.PlayerPower + "\n" + Player.PlayerSword + "\n");
+			pWriter.flush();
+			JOptionPane.showMessageDialog(null , "Datei wurde gespeichert.", "Datei wurde gespeichert!", JOptionPane.INFORMATION_MESSAGE);
+		}catch(IOException ioe){
+			JOptionPane.showMessageDialog(null, "Datei konnte nicht gespeichert werden.", "Errror", JOptionPane.ERROR_MESSAGE, null);
+
 			
 		
 		
